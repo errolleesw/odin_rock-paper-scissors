e3d7501fbc1d817ac678d640919f988ee363a681
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-18 09:43:01 +1100

    add game and isvalidselection functions

--- js/script.js
+++ js/script.js
@@ -8,21 +8,60 @@ function playRound(playerSelection, computerSelection) {
     // convert playerSelection to lowercase for case-insensitive comparison
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-  
+
     if (playerSelection === computerSelection) {
-      return "It's a tie!";
+        return "It's a tie!";
     } else if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-      return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`;
+        return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`;
     } else {
-      return `You Lose! ${computerSelection} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+        return `You Lose! ${computerSelection} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
     }
-  }
+}
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
+
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose Rock, Paper, or Scissors");
+        
+
+        if (!isValidSelection(playerSelection.toLowerCase())) {
+            return "Invalid selection. Please choose Rock, Paper, or Scissors.";
+        }
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+
+        console.log(result);
+
+        if (result.startsWith("You Win!")) {
+            playerScore++;
+        } else if (result.startsWith("You Lose!")) {
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`Congratulations, you win the game! Final score: ${playerScore} - ${computerScore}`);
+    } else if (playerScore < computerScore) {
+        console.log(`Sorry, you lose the game. Final score: ${playerScore} - ${computerScore}`);
+    } else {
+        console.log(`It's a tie! Final score: ${playerScore} - ${computerScore}`);
+    }
+}
+
+function isValidSelection(selection) {
+    const validChoices = ["rock", "paper", "scissors"];
+    return validChoices.includes(selection);
+}
 
