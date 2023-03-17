40fed9aede2081d0f0cc5b4fe1b62edc756f0289
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-18 09:35:08 +1100

    add playRound function to script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,27 @@ function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
-  }
+}
+
+function playRound(playerSelection, computerSelection) {
+    // convert playerSelection to lowercase for case-insensitive comparison
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
   
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`;
+    } else {
+      return `You Lose! ${computerSelection} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+    }
+  }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
